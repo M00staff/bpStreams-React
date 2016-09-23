fa2cbd8a14187996557c4f5245ab4c63df76a4ec
M00staff <Mostafa@nete.com>
2016-09-23 10:45:41 -0400

    backend and frontend are linked and talking to each other, just have to figure out how to pass the year and row arguments and retun in a nice JSON format

--- App.js
+++ App.js
@@ -1,15 +1,111 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
+import fetchJsonp from 'fetch-jsonp';
+// const style = require('./styles.css')
 
 
 class App extends React.Component {
   render() {
-    return (
-      <h1>BP Streams</h1>
+    return(
+    <div>
+      <div className="column column-12">
+          <ul>
+            <li>
+              <img src="http://mnmpresents.com/wp-content/uploads/2010/07/bpLogo.jpg" />
+            </li>
+          </ul>
+      </div>
+
+      <Years />
+
+        <div className="column column-12 footnote">
+          <a href="mailto:MostafaSHabib@gmail.com">Questions and Site Issues can be sent to MostafaSHabib@gmail.com</a>
+        </div>
+    </div>
     )
   }
 }
+export { App };
 
-export { App }
+
+
+
+
+
+class Years extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+
+  pickYear(year, row) {
+
+    fetch('/years').then(console.log('frontend click'))
+
+  }
+
+  //     const request = new Request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ year +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ row +'&page=1&output=json&callback=JSONP_CALLBACK', {
+  //     	method: 'GET',
+  //     	mode: 'no-cors',
+  //     	redirect: 'follow',
+  //     	headers: new Headers({
+  //     		'Content-Type': 'application/json'
+  //     	})
+  //     });
+  //
+  // fetch(request).then(function(response) {
+  //   console.log(response);
+  // });
+
+
+        // method: "GET",
+        // mode: "no-cors",
+        // headers: {
+        //   'Access-Control-Allow-Origin': 'https://archive.org/advancedsearch.php'
+        // }
+        // ,
+        // body: 'q=BrothersPast,%20year:'+ year +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ row +'&page=1&output=json&callback=JSONP_CALLBACK'
+      // ).then(response => {
+      //   console.log(response);
+      // })
+
+
+
+      // ((res: Response) => {
+      //   // subscribe also allows us to change observable to JSON
+      //   this.shows = res.json();
+      //   console.log(this.shows);
+      //   // grabs and attaches filtered data correct
+      //   this.showList = this.shows.response.docs
+      //   console.log(this.showList)
+      //
+      // })
+
+  render() {
+    return (
+      <div>
+        <div className="column column-12">
+          <div className="yearButtons">
+          <p>Select Year</p>
+            <a href="#/">
+                <button onClick={ this.pickYear.bind(this, 2001, 15) } >2001</button>
+                <button onClick={ this.pickYear } >2002</button>
+                <button>2003</button>
+                <button>2004</button>
+                <button>2005</button>
+                <button>2006</button>
+                <button>2007</button>
+                <button>2008</button>
+                <button>2009</button>
+                <button>2010</button>
+                <button>2011</button>
+                <button>2012</button>
+                <button>2013</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ server/server.js
@@ -0,0 +1,52 @@
+// server.js
+
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var path           = require('path');
+// var bodyParser     = require('body-parser');
+
+// configuration ===========================================
+
+// link to database connection if ever need
+// var db = require('./config/db');
+
+// set port
+var port = process.env.PORT || 8080;
+
+// connect to database if ever need
+// (uncomment after you enter in your own credentials in config/db.js)
+// mongoose.connect(db.url);
+
+// get all data/stuff of the body (POST) parameters
+// parse application/json
+// app.use(bodyParser.json());
+
+// parse application/vnd.api+json as json
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// console.log( path.join(__dirname, '../' ));
+
+
+//         ------------- expose WHOLE project
+app.use(express.static(path.join(__dirname, '../')));
+
+
+app.get('/years', function(req, res) {
+  console.log('server side clicked');
+  return res.jsonp('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json&callback=JSONP_CALLBACK')
+})
+
+// catch all route
+app.get('*', function(req, res) {
+    res.sendFile('index.html', { root: './'}); // load our public/index.html file
+});
+
+// start app ===============================================
+// startup our app at http://localhost:8080
+app.listen(port);
+
+// log port
+console.log('listening on port ' + port);
+
+// expose app
+exports = module.exports = app;
