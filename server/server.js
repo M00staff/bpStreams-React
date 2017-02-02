deae6584ab0f9810b8c9c8c4c9f32acc0e4c52c4
M00staff <Mostafa@nete.com>
2017-02-02 15:17:15 -0500

    added Request library to handle HTTP requests - it works on the backend, need to get res to FE

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@
 var express        = require('express');
 var app            = express();
 var path           = require('path');
+// 3rd party library for handling HTTP requests 
+var request        = require('request');
 // var bodyParser     = require('body-parser');
 
 // configuration ===========================================
@@ -30,12 +32,21 @@ var port = process.env.PORT || 8080;
 //         ------------- expose WHOLE project
 app.use(express.static(path.join(__dirname, '../')));
 
-
+// request from front end
+// response to front end
 app.get('/years', function(req, res) {
   console.log('server side clicked')
-  req.get('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json')
-  return res.json()
+  // req.get('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json&callback=JSONP_CALLBACK')
+  // return res.send('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json&callback=JSONP_CALLBACK')
+  request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json&callback=JSONP_CALLBACK', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Show the response
+    res.send({body})
+  }
 })
+
+})
+
 
 // catch all route
 app.get('*', function(req, res) {
