e98729f7fae45e65cd63ddfc0f1c04abf7f47b2d
Mostafa Habib <MostafaSHabib@gmail.com>
2017-02-11 21:36:59 -0500

    backend working and passing correct data back to frontend

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ class Years extends React.Component {
   pickYear(year, row) {
 
     // we HAVE to handle requests on the back end because of CORS
-    fetch(`./years?year=${year}`)
+    fetch(`./years?year=${year}&row=${row}`)
     .then(response => response.json()).then(json => {
       console.log(json);
     });
@@ -115,18 +115,18 @@ class Years extends React.Component {
           <p>Select Year</p>
             <a href="#/">
                 <button onClick={ this.pickYear.bind(this, 2001, 15) } >2001</button>
-                <button onClick={ this.pickYear } >2002</button>
-                <button>2003</button>
-                <button>2004</button>
-                <button>2005</button>
-                <button>2006</button>
-                <button>2007</button>
-                <button>2008</button>
-                <button>2009</button>
-                <button>2010</button>
-                <button>2011</button>
-                <button>2012</button>
-                <button>2013</button>
+                <button onClick={ this.pickYear.bind(this, 2002, 26) } >2002</button>
+                <button onClick={ this.pickYear.bind(this, 2003, 84) } >2003</button>
+                <button onClick={ this.pickYear.bind(this, 2004, 96) } >2004</button>
+                <button onClick={ this.pickYear.bind(this, 2005, 131) } >2005</button>
+                <button onClick={ this.pickYear.bind(this, 2006, 84) } >2006</button>
+                <button onClick={ this.pickYear.bind(this, 2007, 25) } >2007</button>
+                <button onClick={ this.pickYear.bind(this, 2008, 4) } >2008</button>
+                <button onClick={ this.pickYear.bind(this, 2009, 17) } >2009</button>
+                <button onClick={ this.pickYear.bind(this, 2010, 20) } >2010</button>
+                <button onClick={ this.pickYear.bind(this, 2011, 34) } >2011</button>
+                <button onClick={ this.pickYear.bind(this, 2012, 14) } >2012</button>
+                <button onClick={ this.pickYear.bind(this, 2013, 1) } >2013</button>
             </a>
           </div>
         </div>
--- server/server.js
+++ server/server.js
@@ -35,17 +35,13 @@ app.use(express.static(path.join(__dirname, '../')));
 // request from front end
 // response to front end
 app.get('/years', function(req, res) {
-  // get me working!
-  console.log(req.query)
-  // req.get('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json&callback=JSONP_CALLBACK')
-  // return res.send('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json&callback=JSONP_CALLBACK')
-  request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ 2001 +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ 15 +'&page=1&output=json', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body) // Show the response
-    res.send(body)
-  }
-})
-
+  request('https://archive.org/advancedsearch.php?q=BrothersPast,%20year:'+ req.query.year +'&fl%5B%5D=year&fl%5B%5D=date&fl%5B%5D=identifier,title&sort%5B%5D=&sort%5B%5D=&sort%5B%5D=&rows='+ req.query.row +'&page=1&output=json',
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log(body) // Show the response
+        res.send(body)
+      }
+  })
 })
 
 
