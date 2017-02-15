b9731389490e1c3a8bcb1ea5944a5ef59f48ec0c
M00staff <Mostafa@nete.com>
2017-02-15 12:07:27 -0500

    added react audio player, it works perfectly

--- client/Songs.js
+++ client/Songs.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 
 class Songs extends React.Component {
 
+  constructor() {
+    super();
+    this.state = { track: null }
+  }
 
   componentWillMount() {
-    console.log(this.props.setList);
+    // console.log(this.props.setList);
   }
 
 
   songPick(song) {
     console.log(song);
+    this.setState({ track: song.songSource })
   }
 
 
@@ -26,8 +32,10 @@ class Songs extends React.Component {
     return (
       <div className='allSongsContainer'>{ this.renderSongs(this.props.setList) }
 
-          <span className="player-song-title"></span>
-          <audio controls autoPlay="true" className="player" preload="auto"></audio>
+          <ReactAudioPlayer
+            src={ this.state.track }
+            autoPlay
+          />
 
       </div>
     )
