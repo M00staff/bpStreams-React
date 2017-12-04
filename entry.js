ea537ea5f03c7f80fdbe979cfac1bd1bf3ecfe0a
Mostafa Habib <mhabib@ALTBCG8WL.fios-router.home>
2017-12-04 18:44:31 -0500

    added redux logger

--- entry.js
+++ entry.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import rootReducer from './client/redux/reducers';
 import App from './client/App';
 import './styles.css';
 
 // Wrap the app in Provider and create the Redux Store in the Entry Point
 /* eslint-disable no-underscore-dangle */
-const store = createStore(rootReducer, /* preloadedState, */
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer,
+  /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger),
+);
 /* eslint-enable */
 
 ReactDOM.render(
